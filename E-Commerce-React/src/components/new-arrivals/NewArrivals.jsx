28b67b49226ad7538d331ee6bb2d938428e63956
Kundan <kundantikale4@gmail.com>
2025-01-16 13:46:13 +0530

    Data Featched

--- E-Commerce-React/src/components/new-arrivals/NewArrivals.jsx
+++ E-Commerce-React/src/components/new-arrivals/NewArrivals.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./newArrivals.css";
 
 const NewArrivals = () => {
-  return (
-    <div>NewArrivals</div>
-  )
-}
+  const [products, setProducts] = useState([]);
 
-export default NewArrivals
+
+  async function getProducts() {
+    try {
+      const response = await fetch("https://dummyjson.com/products?limit=10");
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const data = await response.json();
+      setProducts(data.products);
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+
+  return (
+    <section className="cards-container">
+      <div className="card-heading">
+        <h1>NEW ARRIVALS</h1>
+      </div>
+      <div className="card-body">
+        {products.map((product) => (
+          <div className="card-item" key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>${product.price}</p>
+            <button>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+      <div className="view-all-btn">
+        <button className="show-all">View All</button>
+      </div>
+    </section>
+  );
+};
+
+export default NewArrivals;
--- E-Commerce-React/src/pages/Home.jsx
+++ E-Commerce-React/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main from '../components/main/Main'
+import NewArrivals from '../components/new-arrivals/NewArrivals'
 
 const Home = () => {
   return (
     <> 
     <Main></Main>
+    <NewArrivals></NewArrivals>
     </>
   )
 }
